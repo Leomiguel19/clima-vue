fe9649217c0d3bafec98519fcdc6da8e731cf498
Leonardo Guilarte <leomiguel1907@gmail.com>
2024-03-26 23:29:47 -0400

    Primer llamado API

--- src/composables/UseClima.js
+++ src/composables/UseClima.js
@@ -4,7 +4,9 @@ export default function useClima(){
         // Importar el API KEY
         const key = import.meta.env.VITE_API_KEY;
         // Obtener la lat, lng
+        const url =`http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`
         // Obtener el clima
+        console.log(url);
     }
 
     return {
