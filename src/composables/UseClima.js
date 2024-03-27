147210b21f14af5031c9d41996a7a9d844ba48e4
Leonardo Guilarte <leomiguel1907@gmail.com>
2024-03-26 23:36:16 -0400

    Obtener latitud y longitud con axios

--- src/composables/UseClima.js
+++ src/composables/UseClima.js
@@ -1,12 +1,22 @@
+import axios from 'axios';
+
 export default function useClima(){
 
-    const obtenerClima = ({ciudad, pais}) => {
+    const obtenerClima = async ({ciudad, pais}) => {
         // Importar el API KEY
         const key = import.meta.env.VITE_API_KEY;
-        // Obtener la lat, lng
-        const url =`http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`
-        // Obtener el clima
-        console.log(url);
+
+        try {
+            // Obtener la lat, lng
+            const url =`http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`
+            const {data} = await axios(url);
+            const { lat, lon } = data[0];
+            // Obtener el clima
+            
+        } catch (error) {
+            console.log(error);
+        }
+
     }
 
     return {
