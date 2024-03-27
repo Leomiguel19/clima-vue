a50ecaf94d2536da08fee2722f4d2c0b47cc7e9c
Leonardo Guilarte <leomiguel1907@gmail.com>
2024-03-26 23:43:44 -0400

    Consultar el clima

--- src/composables/UseClima.js
+++ src/composables/UseClima.js
@@ -12,7 +12,9 @@ export default function useClima(){
             const {data} = await axios(url);
             const { lat, lon } = data[0];
             // Obtener el clima
-            
+            const urlCLima = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`;
+            const {data: dataClima} = await axios(urlCLima);
+            console.log(dataClima);
         } catch (error) {
             console.log(error);
         }
